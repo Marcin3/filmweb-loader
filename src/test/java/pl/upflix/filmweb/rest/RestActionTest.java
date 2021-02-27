72343e3edc50909650670dc09ea8e5b824422473
masf@softwaremind.com <masf@softwaremind.com>
2021-02-27 18:12:16 +0100

    Fix tests

--- src/test/java/pl/upflix/filmweb/rest/RestActionTest.java
+++ src/test/java/pl/upflix/filmweb/rest/RestActionTest.java
@@ -9,27 +9,27 @@ import org.testng.annotations.Test;
 public class RestActionTest {
 	private static final String URL_MYSTIC_RIVER = "https://upflix.pl/Mystic River";
 	private RestAction restAction;
-	
+
 	@BeforeClass
 	public void setUp() {
 		restAction = new RestAction();
 	}
-	
+
 	@Test
 	public void createURLTest() {
 		then(restAction.createURL("Mystic River"))
 				.isEqualTo(URL_MYSTIC_RIVER);
 	}
-	
+
 	@DataProvider(name = "correctResponse")
 	public Object[][] createData1() {
 		return new Object[][]{
-				{"Znaleziono:"},
+				{"znalezionych tytułów: 1"},
 				{"Lista filmów i seriali VOD w Polsce | Najnowsze zmiany"},
-				{"Wszystkie aktualności upflix.pl | Netflix | Showmax | HBO GO | Amazon Prime Video"},
+				{"Wszystkie aktualności upflix.pl | Netflix | HBO GO | Amazon Prime Video | Chili | Cineman | Ipla | Rakuten | iTunes | Player | VOD.pl | TVP VOD | Apple TV+ | PLAY NOW"},
 		};
 	}
-	
+
 	@Test(dataProvider = "correctResponse")
 	public void getHtmlFromURLTest(final String expectedText) {
 		then(restAction.getHtmlFromURL(URL_MYSTIC_RIVER))
